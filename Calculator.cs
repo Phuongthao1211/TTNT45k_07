e66e00a8f9422be1a473b5dc14bc03550b9e2d37
Phan Thị Phương Thảo <85329771+Phuongthao1211@users.noreply.github.com>
2021-06-09 00:35:31 +0700

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cong_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txt_a.Text);
+            int b = Convert.ToInt32(txt_b.Text);
+
+            txt_kq.Text = "" + (a + b);
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            txt_a.Text = "";
+            txt_b.Text = "";
+            txt_kq.Text = "";
+        }
+
+        private void btn_dong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_tru_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txt_a.Text);
+            int b = Convert.ToInt32(txt_b.Text);
+
+            txt_kq.Text = "" + (a - b);
+        }
+
+        private void btn_nhan_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txt_a.Text);
+            int b = Convert.ToInt32(txt_b.Text);
+
+            txt_kq.Text = "" + (a * b);
+        }
+
+        private void btn_chia_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txt_a.Text);
+            double b = Convert.ToDouble(txt_b.Text);
+            if (b == 0)
+            {
+                txt_kq.Text = "Lỗi";
+            }
+            else
+            {
+
+                txt_kq.Text = "" + (a / b);
+            }
+        }
+    }
+}
