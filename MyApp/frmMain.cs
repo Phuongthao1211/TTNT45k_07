bbb0384756fb179c880e0ad26a3ec91d2b9b403b
dochitoan <dochitoan28201@gmail.com>
2021-06-19 23:48:55 +0700

    Create frmMain.cs

---
+++ MyApp/frmMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+      
+            private void thêmMớiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmDK khach = new frmDK();
+            khach.MdiParent = this;
+            khach.Show();
+
+        }
+
+        private void thêmMớiToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmTS thisinh = new frmTS();
+            thisinh.MdiParent = this;
+            thisinh.Show();
+        }
+
+        private void thốngKêNgườiĐăngKíToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmThongKe danhsach = new frmThongKe();
+            danhsach.MdiParent = this;
+            danhsach.Show();
+        }
+
+        private void thốngKêCácKìThiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmThongKe danhsach = new frmThongKe();
+            danhsach.MdiParent = this;
+            danhsach.Show();
+        }
+    }
+}
