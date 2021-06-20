6569587d61013f87837b08fa0415d28a39311d28
Phan Thị Phương Thảo <85329771+Phuongthao1211@users.noreply.github.com>
2021-06-20 14:59:53 +0700

    Create KI_THI.sql

---
+++ KI_THI.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [MaKT]
+      ,[PhongThi]
+      ,[NgayThi]
+      ,[GioThi]
+      ,[SoLuongToiDa]
+      ,[SoLuongDK]
+  FROM [QLDANGKYTHI].[dbo].[KI_THI]
