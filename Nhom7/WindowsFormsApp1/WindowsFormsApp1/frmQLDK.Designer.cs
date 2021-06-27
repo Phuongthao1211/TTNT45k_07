a3b6be1a486e822c2e25a9465c8ceb938e34d243
Phan Thị Phương Thảo <85329771+Phuongthao1211@users.noreply.github.com>
2021-06-27 20:19:41 +0700

    Create frmQLDK.Designer.cs

---
+++ Nhom7/WindowsFormsApp1/WindowsFormsApp1/frmQLDK.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frmQLDK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQLDK));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtMKT = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLP = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtngaydk = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.dtgvDK = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttukhoa = new System.Windows.Forms.TextBox();
+            this.btntim = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(159, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "SỐ ĐT:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(159, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "MÃ KÌ THI:";
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(306, 86);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(236, 20);
+            this.txtSDT.TabIndex = 16;
+            // 
+            // txtMKT
+            // 
+            this.txtMKT.Location = new System.Drawing.Point(306, 124);
+            this.txtMKT.Name = "txtMKT";
+            this.txtMKT.Size = new System.Drawing.Size(236, 20);
+            this.txtMKT.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(159, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "LỆ PHÍ:";
+            // 
+            // txtLP
+            // 
+            this.txtLP.Location = new System.Drawing.Point(305, 201);
+            this.txtLP.Name = "txtLP";
+            this.txtLP.Size = new System.Drawing.Size(237, 20);
+            this.txtLP.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(159, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "NGÀY ĐĂNG KÍ:";
+            // 
+            // dtngaydk
+            // 
+            this.dtngaydk.Location = new System.Drawing.Point(306, 160);
+            this.dtngaydk.Name = "dtngaydk";
+            this.dtngaydk.Size = new System.Drawing.Size(237, 20);
+            this.dtngaydk.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(228, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 31);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "QUẢN LÍ ĐĂNG KÍ";
+            // 
+            // btnluu
+            // 
+            this.btnluu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnluu.ForeColor = System.Drawing.Color.Blue;
+            this.btnluu.Location = new System.Drawing.Point(200, 236);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(126, 41);
+            this.btnluu.TabIndex = 27;
+            this.btnluu.Text = "CHỈNH SỬA";
+            this.btnluu.UseVisualStyleBackColor = true;
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxoa.ForeColor = System.Drawing.Color.Blue;
+            this.btnxoa.Location = new System.Drawing.Point(398, 236);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(109, 41);
+            this.btnxoa.TabIndex = 29;
+            this.btnxoa.Text = "XÓA";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // dtgvDK
+            // 
+            this.dtgvDK.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvDK.Location = new System.Drawing.Point(109, 342);
+            this.dtgvDK.Name = "dtgvDK";
+            this.dtgvDK.Size = new System.Drawing.Size(563, 177);
+            this.dtgvDK.TabIndex = 30;
+            this.dtgvDK.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvDK_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(231, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 15);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Nhập số ĐT:";
+            // 
+            // txttukhoa
+            // 
+            this.txttukhoa.Location = new System.Drawing.Point(333, 314);
+            this.txttukhoa.Name = "txttukhoa";
+            this.txttukhoa.Size = new System.Drawing.Size(236, 20);
+            this.txttukhoa.TabIndex = 32;
+            // 
+            // btntim
+            // 
+            this.btntim.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntim.ForeColor = System.Drawing.Color.Blue;
+            this.btntim.Location = new System.Drawing.Point(578, 306);
+            this.btntim.Name = "btntim";
+            this.btntim.Size = new System.Drawing.Size(94, 32);
+            this.btntim.TabIndex = 33;
+            this.btntim.Text = "Tìm ";
+            this.btntim.UseVisualStyleBackColor = true;
+            this.btntim.Click += new System.EventHandler(this.btntim_Click);
+            // 
+            // frmQLDK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._48382820_322179758623756_4834527527542194176_n5;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 531);
+            this.Controls.Add(this.btntim);
+            this.Controls.Add(this.txttukhoa);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtgvDK);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtngaydk);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtLP);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMKT);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmQLDK";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lí đăng kí";
+            this.Load += new System.EventHandler(this.frmQLDK_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtMKT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLP;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtngaydk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.DataGridView dtgvDK;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttukhoa;
+        private System.Windows.Forms.Button btntim;
+    }
+}
